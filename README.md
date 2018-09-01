8af52c9cde3e8195c064747f78b8ba1d6cd2689a
Lohith G N <kashyapa78@hotmail.com>
2018-09-01 23:58:22 +0530

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # blazor-examples
-A repo which contains live examples of using Blazor
+A repo which contains live examples of using Blazor.
+
+# Working with the code base
+
+## 1. Pre-Requisites:
+
+### Get Blazor 0.5.1
+To get setup with Blazor 0.5.1:
+1. Install the .NET Core 2.1 SDK (2.1.300 or later).
+2. Install Visual Studio 2017 (15.7) with the ASP.NET and web development workload selected. 
+3. Install the latest Blazor Language Services extension from the Visual Studio Marketplace.
+4. To install the Blazor templates on the command-line:
+```
+dotnet new -i Microsoft.AspNetCore.Blazor.Templates
+```
+
+You can find getting started instructions, docs, and tutorials for Blazor at https://blazor.net.
+
+## 2. Clone source code
+Clone the source code on your machine. Open `src\Blazamples.sln` in Visual Studio. Start hacking.
+
---
+++ src/Blazamples/wwwroot/js/codeViewerExtensions.js
@@ -0,0 +1,32 @@
+﻿const modelTabHeaderId = "nav-model-tab";
+const modelTabContentId = "nav-model";
+const viewTabHeaderId = "nav-view-tab";
+const viewTabContentId = "nav-view";
+
+function setVisibility(isVisible, tabHeaderId, tabContentId) {
+    var modelTabHeader = document.getElementById(tabHeaderId);
+    var modelTabContent = document.getElementById(tabContentId);
+    if (!isVisible)
+    {
+        modelTabHeader.classList.remove("active");
+        modelTabContent.classList.remove("show");
+        modelTabContent.classList.remove("active");
+    }
+    else
+    {
+        modelTabHeader.classList.add("active");
+        modelTabContent.classList.add("show");
+        modelTabContent.classList.add("active");
+    }
+}
+
+window.codeViewerExtensions = {
+    showView: function () {
+        setVisibility(false, modelTabHeaderId, modelTabContentId);
+        setVisibility(true, viewTabHeaderId, viewTabContentId);
+    },
+    showModel: function () {
+        setVisibility(false, viewTabHeaderId, viewTabContentId);
+        setVisibility(true, modelTabHeaderId, modelTabContentId);
+    }
+}
